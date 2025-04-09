439181746803486c0feb84f9baf2de0be9cd14cc
BotchaVarun <varunbotcha@gmail.com>
2025-04-09 18:32:44 +0530

    backend update

--- server/index.js
+++ server/index.js
@@ -17,6 +17,18 @@ app.use("/api", router);
 
 const port = 8080;
 
+
+
+app.use(cors());
+
+
+const corsOptions = {
+    origin: 'https://todo-lac-five-94.vercel.app', 
+    methods: 'GET, POST, PUT, DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+};
+
 async function startServer() {
     await connectToMongoDB();
     app.listen(port, () => {
