57a11ab9b75ce71999ad24d8c07ffe2ba0e131a3
BotchaVarun <varunbotcha@gmail.com>
2025-04-09 19:00:23 +0530

    backend update

--- server/database.js
+++ server/database.js
@@ -2,13 +2,7 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const uri = "mongodb+srv://varunbotcha:varun123@cluster0.afzwir6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://todo-lac-five-94.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-});
+
 const express=require('express');
 const app = express();
 app.use(express.json());
@@ -19,6 +13,13 @@ const options = {
         deprecationErrors: true,
     }
 };
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://todo-lac-five-94.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 app.options('*', cors());
 let client;
 const connectToMongoDB = async () => {
