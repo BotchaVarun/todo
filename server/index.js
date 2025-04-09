4d68820e4e3e2374d436001fe9ee2e9efe9f9e34
BotchaVarun <varunbotcha@gmail.com>
2025-04-09 18:36:11 +0530

    backend update

--- server/index.js
+++ server/index.js
@@ -16,14 +16,14 @@ const router = require("./routes");
 app.use("/api", router);
 
 const port = 8080;
+const cors = require('cors');
 
-
-
+// Enable CORS for all requests
 app.use(cors());
 
-
+// OR, configure CORS with specific options
 const corsOptions = {
-    origin: 'https://todo-lac-five-94.vercel.app', 
+    origin: 'https://todo-lac-five-94.vercel.app', // Allow requests only from this origin
     methods: 'GET, POST, PUT, DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
